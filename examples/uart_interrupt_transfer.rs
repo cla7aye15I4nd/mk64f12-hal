07936e6678912f72fe4dbaba85f4123c17ead4e5
dataisland <dataisland@outlook.com>
2022-11-07 23:36:49 -0600

    Fix some basic features

--- examples/uart_interrupt_transfer.rs
+++ examples/uart_interrupt_transfer.rs
@@ -6,10 +6,13 @@
 
 use panic_halt as _;
 
+use mk64f12_hal as hal;
 use cortex_m_rt::entry;
+
 
 #[entry]
 fn main() -> ! {
-    
+    let mut peripherals = hal::Peripherals::take().unwrap();
+
     loop {}
 }
